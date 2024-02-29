f177dccc83acca57a248f17aa805a8aff0f62474
niteshgurjar94 <niteshgurjar94@gmail.com>
2024-02-29 18:50:29 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,106 @@
+public class MyClass
+{
+    public static void main (String[] args)
+    {
+        //int x = 50;
+        //float y = 40;
+        /*if (x==y)
+        {
+            System.out.println ("1");    
+        }
+        else if (x<y)
+        {
+            System.out.println ("2");
+        }
+        else
+        {
+            System.out.println ("3");
+        }*/
+        
+        //Code for if-else statements without using if else:
+        /*int time = 17;
+        String result = (time < 18 ) ? "Good Day.": "Good Evening.";
+        System.out.println(result);*/
+        
+        //Switch case for day:
+        
+        /*int day = 4;
+        switch (day)
+        {
+            case 1 :
+                System.out.println ("Saturday");
+                break;
+            case 2 :
+                System.out.println ("Sunday");
+                break;
+            case 3 :
+                System.out.println ("Monday");
+                break;
+            default :
+                System.out.println ("Weekend");
+        } */
+        
+        
+        //While loop conditions:
+        
+        /*int i = 0;
+        while (i<5)
+        {
+            System.out.println(i);
+            i++;
+        }*/
+        
+        //do while loop conditions:
+       /* int i = 0;
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while (i<5); */
+        
+        
+        // For loops
+        
+        //String[] cars = {"Volvo","Alto","BMW"};
+        //for (String i : cars) {
+            //System.out.println(i);
+            
+        // For loops using counting
+        /*int i;
+        for ( i=10; i>=0; i = i-2)
+        {
+            System.out.println(i);
+        }*/
+        
+        //Nested loop
+        
+        /*int i,j,k;
+        for (i=1; i<=5; i++)
+        {
+            for (j=4; j>=i; j--)
+            {
+                System.out.print(" ");
+            }
+            for (k=1;k<=(2*i-1);k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }*/
+        
+        //Break/Continue
+        
+        /*int i;
+        for(i=0;i<=10;i++)
+        {
+            if(i==5)
+            {
+                continue ; //break
+            }
+            System.out.println(i);
+        }
+        */
+        
+        
+    }
+}
